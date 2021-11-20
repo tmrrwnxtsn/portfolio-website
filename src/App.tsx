36a85b638d690ace9dd86442fb7f1ec5db8af9ef
Pavel Kurmyza <tmrrwnxtsn@gmail.com>
2021-11-20 14:22:35 +0400

    Implement additional task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Counter from "./Counter";
 
 function App() {
+    const [stepCounter, setStepCounter] = useState(0)
+
     return (
         <div className="App">
-            <Counter start={0} step={1}/>
+            <Counter start={0} step={1} onChange={setStepCounter}/>
+            <Counter start={0} step={stepCounter}/>
         </div>
     );
 }
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Button from './Button';
 
 interface CounterProps {
     start: number;
     step: number;
+    onChange?: (val: number) => any;
 }
 
 const Counter = (props: CounterProps) => {
     const [count, setCount] = useState(props.start);
+
+    useEffect(() => {
+            if (props.onChange) {
+                props.onChange(count);
+            }
+        },
+        [count])
+
     const handleIncrease = () => {
         setCount(count + props.step);
     };
+
     return (
         <div>
             <div>Значение счётчика: {count}.</div>
